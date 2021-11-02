bca5e4f086e853dc6666a5912faca1c0d5fcdca4
mcswift <develop@mcswift.pro>
2021-11-02 13:43:08 +0800

    更新关于本站

---
+++ src/client/components/RemoteLogo.vue
@@ -0,0 +1,33 @@
+ <script setup lang="ts">
+
+const props = defineProps({
+  src:{ 
+    type:String,
+    required:true,
+  }
+})
+
+
+</script>
+<template>
+  <div class="mcswift-icon-container">
+    <img class="mcswift-icon" :src="props.src">
+  </div>
+</template>
+<style>
+.mcswift-icon-container{
+  display: inline-flex;
+  height: 100%;
+  font-size: inherit;
+  min-width: 1em;
+  vertical-align: middle;
+}
+.mcswift-icon{
+  display:inline-block;
+  margin: auto;
+  height: 1em;
+  line-height: 1em;
+  font-size: inherit;
+  /* vertical-align: middle; */
+}
+</style>
--- src/client/pages/about.vue
+++ src/client/pages/about.vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup>
 import type {Store} from "../../../interface"
-import { NA, NH2,NH3, NUl, NLi} from "naive-ui"
+import { NA, NH2,NH3, NUl, NLi, NIcon} from "naive-ui"
+import RemoteLogo from "@/components/RemoteLogo.vue"
 import { inject } from "vue";
 const store = inject("custoStore") as Store;
 store.title = "关于本站"
@@ -12,16 +13,13 @@ store.title = "关于本站"
       <n-h3>技术栈</n-h3>
       <n-ul>
         <n-li>
+          <remote-logo src="https://vuejs.org/images/logo.png"/>&nbsp;
           <n-a href="https://v3.cn.vuejs.org/" target="_blank">
             Vue 3
           </n-a>
         </n-li>
         <n-li>
-          <n-a href="https://v3.nuxtjs.org/" target="_blank">
-            Nuxt 3
-          </n-a>
-        </n-li>
-        <n-li>
+          <remote-logo src="https://naiveui.oss-cn-hongkong.aliyuncs.com/naivelogo.svg"/>&nbsp;
           <n-a href="https://www.naiveui.com/" target="_blank">
             Naive UI
           </n-a>
@@ -29,6 +27,24 @@ store.title = "关于本站"
         <n-li>
           <n-a href="https://www.xicons.org/" target="_blank">
             xicons
+          </n-a>
+        </n-li>
+        <n-li>
+          <remote-logo src="https://vitejs.dev/logo.svg"/>&nbsp;
+          <n-a href="https://cn.vitejs.dev/" target="_blank">
+            Vite
+          </n-a>
+        </n-li>
+        <n-li>
+          <remote-logo src="https://vitejs.dev/logo.svg"/>&nbsp;
+          <n-a href="https://github.com/antfu/vite-plugin-md" target="_blank">
+            vite-plugin-md
+          </n-a>
+        </n-li>
+        <n-li>
+          <remote-logo src="https://vitejs.dev/logo.svg"/>&nbsp;
+          <n-a href="https://github.com/antfu/vite-ssg" target="_blank">
+            vite-ssg
           </n-a>
         </n-li>
       </n-ul>
@@ -49,6 +65,21 @@ store.title = "关于本站"
         <n-li>
           <n-a href="https://www.cloudflare.com/zh-cn/dns/" target="_blank">
             Cloudflare DNS
+          </n-a>
+        </n-li>
+      </n-ul>
+    </div>
+    <div style="margin-top: 40px">
+      <n-h3>曾使用</n-h3>
+      <n-ul>
+        <n-li>
+          <n-a href="https://workers.cloudflare.com/" target="_blank">
+            Cloudflare Workers
+          </n-a>
+        </n-li>
+        <n-li>
+          <n-a href="https://v3.nuxtjs.org/" target="_blank">
+            Nuxt 3
           </n-a>
         </n-li>
       </n-ul>
