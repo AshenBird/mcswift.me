48ac4ac46533f19415d517a49c48d8b266b4eb34
mcswift <develop@mcswift.pro>
2021-11-10 18:41:08 +0800

    更新素材

---
+++ drafts/linux_tree.md
@@ -0,0 +1,88 @@
+# Linux 发行版族谱
+
+## [Arch Linux](https://wiki.archlinux.org/title/Arch_Linux_(%E7%AE%80%E4%BD%93%E4%B8%AD%E6%96%87))
+
+基于社区
+Pacman 包管理
+滚动更新
+
+### Manjaro
+
+说实话我觉得它的桌面挺好看的
+
+## Debian
+
+有个叫 Debian Pkg 的包管理器
+
+### Ubuntu
+
+#### KDE Neon
+
+使用 KDE Plasma 桌面
+
+#### Pop!_OS
+
+#### Linux Mint
+
+#### elementary OS
+
+有一个专属的桌面 贼好看
+
+#### Zorin OS
+
+可以将布局切换为 macOS 风格/ Windows 风格的外观。
+
+#### Nitrux OS
+
+它使用基于 KDE 的 Nomad 桌面
+
+### Tails
+
+据说很安全
+
+### Kali Linux
+
+安全领域占有率高
+
+### antiX
+
+#### MX Linux
+
+它也搭载了 Xfce桌面
+
+### Knoppix
+
+#### MEPIS
+
+它的 Xfce 桌面好看
+
+### Raspberry Pi OS
+
+## Red Hat
+
+### Fedora
+
+有个叫 DNF 的包管理器
+
+### CentOS
+
+### CentOS Stream
+
+据说这个不是 CentOS
+
+## openSUSE
+
+有个叫 zypper 的包管理器
+
+## Gentoo
+
+有个叫 Portage 的包管理器
+
+## Solus
+
+有个叫 Budgie ，据说很受欢迎
+
+## Slackware
+
+Mate
+Gnome
--- src/client/pages/index.vue
+++ src/client/pages/index.vue
@@ -1,57 +1,79 @@
 <script setup lang="ts">
-import type {Store} from "../../../interface"
+import type { Store } from "../../../interface";
 
-import { NSpace, NA, NIcon } from "naive-ui"
-import { NTooltip } from "naive-ui"
+import { NSpace, NA, NIcon } from "naive-ui";
+import { NTooltip } from "naive-ui";
 
-import { ZhihuCircleFilled as ZhihuIcon, GithubFilled as GithubIcon } from "@vicons/antd"
-import { inject } from "vue"
+import {
+  ZhihuCircleFilled as ZhihuIcon,
+  GithubFilled as GithubIcon,
+} from "@vicons/antd";
+import { inject } from "vue";
 
-const store = inject("custoStore") as Store
-store.title = "首页"
-
+const store = inject("custoStore") as Store;
+store.title = "首页";
 </script>
 
 <template>
   <div class="home-container">
     <nav class="home-nav">
       <NSpace justify="center" align="center">
-        <router-link class="home-nav-item"  to="/blog">
-          <NA class="home-nav-item" href="javascript:void(0);" >博客</NA>
+        <router-link class="home-nav-item" to="/blog">
+          <NA class="home-nav-item" href="javascript:void(0);">博客</NA>
         </router-link>
         <div class="home-nav-divider"></div>
+        <!-- <router-link class="home-nav-item" to="/blog">
+          <NA class="home-nav-item" href="javascript:void(0);">杂志</NA>
+        </router-link>
+        <div class="home-nav-divider"></div>
+        <router-link class="home-nav-item" to="/blog">
+          <NA class="home-nav-item" href="javascript:void(0);">清单</NA>
+        </router-link>
+        <div class="home-nav-divider"></div> -->
         <NTooltip trigger="click">
           <template #trigger>
-            <NA class="home-nav-item" href="javascript:void(0);" >作品</NA>
+            <NA class="home-nav-item" href="javascript:void(0);">作品</NA>
           </template>
           <span>施工中...</span>
         </NTooltip>
         <div class="home-nav-divider"></div>
-          <router-link
-            to="/connect"
-            #="{ navigate, href }"
-            custom
-          >
-            <NA class="mcswift-link" :href="href" @click="navigate">
-              联系我
-            </NA>
-          </router-link>
+        <router-link to="/connect" #="{ navigate, href }" custom>
+          <NA class="mcswift-link" :href="href" @click="navigate"> 联系我 </NA>
+        </router-link>
+        <!-- <div class="home-nav-divider"></div>
+        <NTooltip trigger="click">
+          <template #trigger>
+            <NA class="home-nav-item" href="javascript:void(0);">玩具</NA>
+          </template>
+          <span>施工中...</span>
+        </NTooltip> -->
         <div class="home-nav-divider"></div>
-        <router-link class="home-nav-item"  to="/about">
-          <NA class="home-nav-item" href="javascript:void(0);" >关于本站</NA>
+        <router-link class="home-nav-item" to="/about">
+          <NA class="home-nav-item" href="javascript:void(0);">关于本站</NA>
         </router-link>
       </NSpace>
     </nav>
     <nav class="social-media-nav">
-      <NSpace style="width: 100px;" justify="space-between" align="center">
-        <a class="social-media-nav-item" href="https://github.com/AshenBird" target="_blank">
-          <NIcon size="28" :color="store.theme==='dark'?'#ffffff':'#000000'">
-            <GithubIcon/>
+      <NSpace style="width: 100px" justify="space-between" align="center">
+        <a
+          class="social-media-nav-item"
+          href="https://github.com/AshenBird"
+          target="_blank"
+        >
+          <NIcon
+            size="28"
+            :color="store.theme === 'dark' ? '#ffffff' : '#000000'"
+          >
+            <GithubIcon />
           </NIcon>
         </a>
-        <a class="social-media-nav-item" href="https://www.zhihu.com/people/mcswift-pro" target="_blank">
+        <a
+          class="social-media-nav-item"
+          href="https://www.zhihu.com/people/mcswift-pro"
+          target="_blank"
+        >
           <NIcon size="28" color="#0066FF">
-            <ZhihuIcon/>
+            <ZhihuIcon />
           </NIcon>
         </a>
       </NSpace>
@@ -59,32 +81,45 @@ store.title = "首页"
   </div>
 </template>
 <style>
-.home-nav{
+.home-nav {
   font-size: 16px;
 }
-.social-media-nav{
+.social-media-nav {
   margin-top: 20px;
   display: flex;
   justify-content: center;
 }
-.home-nav-divider{
+.home-nav-divider {
   width: 1px;
   height: 2em;
-  background-image: linear-gradient(180deg, rgb(255,255,255),rgb(200,200,170),rgb(200,200,170), rgb(255,255,255));
+  background-image: linear-gradient(
+    180deg,
+    rgb(255, 255, 255),
+    rgb(200, 200, 170),
+    rgb(200, 200, 170),
+    rgb(255, 255, 255)
+  );
 }
 
-.__dark-theme .home-nav-divider{
-  background-image: linear-gradient(180deg, rgb(16,16,20),rgb(100,100,120),rgb(100,100,120), rgb(16,16,20));
+.__dark-theme .home-nav-divider {
+  background-image: linear-gradient(
+    180deg,
+    rgb(16, 16, 20),
+    rgb(100, 100, 120),
+    rgb(100, 100, 120),
+    rgb(16, 16, 20)
+  );
 }
 
-.home-container{
+.home-container {
   align-self: center;
   display: flex;
   justify-content: center;
   flex-direction: column;
   height: 100%;
 }
-.social-media-nav-item, .home-nav-item{
+.social-media-nav-item,
+.home-nav-item {
   text-decoration: none;
 }
 </style>
